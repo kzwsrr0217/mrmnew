382b43f2675713076623809cb172bfacec44a1fc
Zambo Gergely <zgergely0217@gmail.com>
2025-09-30 18:47:59 +0200

    cypress hozzáadva, login test jó

---
+++ frontend/cypress.config.mjs
@@ -0,0 +1,11 @@
+// mrmnew/frontend/cypress.config.ts
+
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
